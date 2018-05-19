283ceac66848b2412360c435003295b700de3f83
andresflorezp <anflo.97@gmail.com>
2018-05-19 09:07:08 -0500

    commit up

--- src/test/java/sistemaMonitoresTest.java
+++ src/test/java/sistemaMonitoresTest.java
@@ -22,7 +22,6 @@
 //import static org.quicktheories.QuickTheory.qt;
 //import static org.quicktheories.generators.Generate.*;
 //import static org.quicktheories.generators.SourceDSL.*;
-//
 ///**
 // *
 // * @author rafaeljimenez
